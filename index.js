426f809ad0b9920e8a156e971c68f877e6ff2020
itscopebd <itscopebd@gmail.com>
2023-07-23 23:18:37 +0600

    User admission process and information added in database Api

--- index.js
+++ index.js
@@ -3,14 +3,21 @@
 const express = require("express");
 const app = express();
 require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion, Collection } = require('mongodb');
+
+app.use(cors())
+app.use(express.json())
+
 
 app.get("/", (req, res) => {
-    res.send("Rofiq")
+  res.send("Rofiq")
 })
 
 
 const uri = `mongodb+srv://${process.env.DB_NAME}:${process.env.DB_PASS}@cluster0.loltiyt.mongodb.net/?retryWrites=true&w=majority`;
+
+
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -23,6 +30,33 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
+    const college_list = client.db("college-find").collection("college-list")
+    const apply_information = client.db("college-find").collection("apply-information")
+
+
+
+    // find college list api
+
+    app.get("/college-list", async (req, res) => {
+      const result = await college_list.find().toArray();
+      res.send(result);
+      // console.log(result)
+
+    })
+
+    // apply information api 
+
+    app.post("/save-apply-info", async (req, res) => {
+
+      const data = req.body;
+      const result = await apply_information.insertOne(data);
+      res.send(result)
+
+    })
+
+
+
+
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
     // Send a ping to confirm a successful connection
@@ -30,7 +64,7 @@ async function run() {
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
@@ -38,6 +72,6 @@ run().catch(console.dir);
 
 
 
-app.listen(3000,()=>{
-    console.log("Server is running! ")
+app.listen(3000, () => {
+  console.log("Server is running! ")
 })
